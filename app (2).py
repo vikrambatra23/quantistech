6210e5e256e751dc530901509052411d26229766
vikrambatra23 <vikrambatra23@gmail.com>
2026-04-24 14:44:30 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -1,5 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 st.set_page_config(
     page_title="Quantis Tech",
@@ -15,7 +16,10 @@ iframe { border: none !important; }
 </style>
 """, unsafe_allow_html=True)
 
-with open("index.html", "r", encoding="utf-8") as f:
+base_dir = os.path.dirname(os.path.abspath(__file__))
+html_path = os.path.join(base_dir, "index.html")
+
+with open(html_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
 components.html(html_content, height=6000, scrolling=True)
